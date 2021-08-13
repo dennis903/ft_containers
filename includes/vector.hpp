6fa7309e78f58a4420d54fa075d3c6707fbce006
이형진 <hyeolee@ihyeongjin-ui-MacBookPro.local>
2021-08-13 19:24:49 +0900

    make iterator

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -2,9 +2,20 @@
 # define VECTOR_HPP
 # include <memory>
 
+namespace ft
+{
 template<typename T, typename _Alloc = std::allocator<T> >
 class	vector
 {
+	typedef T						value_type;
+	typedef _Alloc					allocate_type;
+	typedef size_t					size_type;
+	typedef ptrdiff_t				difference_type; //두 포인터를 뺀 결과
+	typedef value_type&				reference;
+	typedef const value_type&		const_reference;
+	typedef value_type*				pointer;
+	typedef const value_type*		const_pointer;
 	
+};
 }
 #endif
