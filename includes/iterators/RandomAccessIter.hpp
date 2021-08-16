a8a4ab2d6484855345bfdf43fd4b2c6708d9994c
이형진 <hyeolee@ihyeongjin-ui-MacBookPro.local>
2021-08-16 19:24:04 +0900

    iterator

--- includes/iterators/RandomAccessIter.hpp
+++ includes/iterators/RandomAccessIter.hpp
@@ -1,5 +1,7 @@
 #ifndef RANDOMACCESSITER_HPP
 # define RANDOMACCESSITER_HPP
+namespace ft
+{
 template<typename T, typename Pointer, typename Reference>
 class	RandomAccessIter
 {
@@ -17,13 +19,72 @@ class	RandomAccessIter
 		RamdomAccessIter(const pointer arr) { this->_arr = arr; }
 		RandomAccessIter(const RandomAccessIter &_Iter) { *this = _Iter; }
 		~RandomAccessIter() {}
-
 		//대입 연산자(=)
+		current	&operator=(const RandomAccessIter &_Iter)
+		{
+			if (this == &_Iter)
+				return (*this);
+			this->_arr = _Iter._arr;
+			return (*this);
+		}
 		//증가 연산자(후위, 전위, *a++)
+		current &operator++(void)
+		{
+			this->_arr += 1;
+			return (*this);
+		}
+		current &operator++(int)
+		{
+			current temp(this->_arr);
+			this->_arr += 1;
+			return (temp);
+		}
+		// *a++ 나중에 테스트 해보기
 		//감소 연산자(후위, 전위, *a--)
+		current &operator--(void)
+		{
+			this->_arr -= 1;
+			return (*this);
+		}
+		current &operator--(int)
+		{
+			current temp(this->_arr);
+			this->_arr -= 1;
+			return (temp);
+		}
 		//첨자 연산자([])
-		//비교 연산자(==, !=, >, <, <=, >=)
+		value_type &operator[] (const int index)
+		{
+			return (this->_arr[index]);
+		}
 		//참조 연산자(*, ->)
+		reference operator* () const
+		{
+			return (*this->_arr);
+		}
+		pointer operator-> () const
+		{
+			return (this->_arr);
+		}
 		//+, - += -=
+		current operator+(size_type n)
+		{
+			//iterator의 주소를 찍었을 때 다른 주소를 가짐.
+			current temp(*this);
+			temp._arr += n;
+			return (temp);
+		}
+		current operator-(size_type n)
+		{
+			current temp(*this);
+			temp._arr -= n;
+		}
+		current operator+=(size_type n)
+		{
+			this->_arr += n;
+			return (this);
+		}
+		//비교 연산자(==, !=, >, <, <=, >=)
 };
+}
 #endif
